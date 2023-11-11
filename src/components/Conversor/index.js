7cb48ac714b0884b922de0ee2b3e722525ff08c8
marcelypcosta <amarcelypereiracosta@gmail.com>
2023-11-10 23:36:18 -0300

    Correção do código.

--- src/components/Conversor/index.js
+++ src/components/Conversor/index.js
@@ -1,120 +1,118 @@
 import axios from "axios";
+import React, { useState, useEffect } from "react";
 import "../assets/css/style.css";
-import { useState, useEffect } from "react";
 
-function Converter() {
-  const [currencies, setCurrencies] = useState([]);
+
+const Converter = () => {
+  // Estados
   const [fromCurrency, setFromCurrency] = useState("");
   const [toCurrency, setToCurrency] = useState("");
-  const [inputValue, setInputValue] = useState("");
-  const [result, setResult] = useState("");
+  const [fromResult, setFromResult] = useState("");
+  const [toResult, setToResult] = useState("");
   const [history, setHistory] = useState([]);
+  const [countdown, setCountdown] = useState(0);
 
-  const fetchCurrencies = async () => {
+  // Função para converter moeda
+  const convertCurrency = async () => {
     try {
-      const response = await axios.get("https://api.binance.com/api/v3/ticker/price");
-      const data = response.data;
-      setCurrencies(data);
-    } catch (error) {
-      console.log(`Error: ${error}`);
-    }
-  };
+      // buscando as taxas de câmbio da API da Binance
+      const response = await axios.get(
+        `https://api.binance.com/api/v3/ticker/price?symbol=${fromCurrency.toUpperCase()}${toCurrency.toUpperCase()}`
+      );
+      const exchangeRate = response.data.price;
 
-  useEffect(() => {
-    fetchCurrencies();
-    const interval = setInterval(() => {
-      fetchCurrencies();
-    }, 30000);
+      // Definir o valor inicial
+      setFromResult(1);
 
-    return () => clearInterval(interval);
-  }, []);
+      // Iniciar a contagem regressiva
+      setCountdown(30);
 
-  const convertCurrency = () => {
-    const sourceCurrency = currencies.find((currency) => currency.symbol === fromCurrency);
-    const targetCurrency = currencies.find((currency) => currency.symbol === toCurrency);
+      // Configurar intervalo para atualizar a contagem regressiva a cada segundo
+      const countdownInterval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
 
-    if (sourceCurrency && targetCurrency && inputValue) {
-      if (sourceCurrency.symbol !== targetCurrency.symbol) {
-        const sourceRate = sourceCurrency.price;
-        const targetRate = targetCurrency.price;
-        const value = parseFloat(inputValue);
-
-        const conversionResult = (value * targetRate) / sourceRate;
-        setResult(conversionResult.toFixed(2));
-
-        // Save the conversion in history state
-        const conversion = `${value} ${fromCurrency} = ${conversionResult.toFixed(2)} ${toCurrency}`;
+      // Aguardar 30 segundos antes de exibir o resultado e salvar no histórico
+      setTimeout(() => {
+        clearInterval(countdownInterval);
+        setToResult(exchangeRate);
+        const conversion = `${fromCurrency.toUpperCase()} para ${toCurrency.toUpperCase()}: ${exchangeRate}`;
         setHistory([...history, conversion]);
-      } else {
-        setResult("Selecione uma moeda diferente.");
-      }
-    } else {
-      setResult("Preencha todas as informações necessárias.");
+      }, 30000);
+    } catch (error) {
+      console.error("Erro ao converter moeda:", error);
     }
   };
+
+  // Efeito para limpar o intervalo quando a contagem regressiva chega a zero
+  useEffect(() => {
+    let countdownInterval;
+
+    if (countdown > 0) {
+      countdownInterval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    }
+
+    // Limpar o intervalo quando o componente é desmontado ou a contagem regressiva muda
+    return () => {
+      clearInterval(countdownInterval);
+    };
+  }, [countdown]);
 
   return (
-    <div>
-      <div className="container">
-        <h1>Conversor de Moeda</h1>
-        <div className="conversion_container">
-          <div className="select_from">
-            <select
-              className="select_currency"
-              value={fromCurrency}
-              onChange={(e) => setFromCurrency(e.target.value)}
-            >
-              <option value="">Moeda de Origem</option>
-              {currencies.map((currency) => (
-                <option key={currency.symbol} value={currency.symbol}>
-                  {currency.symbol}
-                </option>
-              ))}
-            </select>
-
-            <input
-              className="input_value"
-              type="number"
-              placeholder="digite o valor aqui"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-          </div>
-
-          <div className="select_to">
-            <select
-              className="select_currency"
-              value={toCurrency}
-              onChange={(e) => setToCurrency(e.target.value)}
-            >
-              <option value="">Moeda de Destino</option>
-              {currencies.map((currency) => (
-                <option key={currency.symbol} value={currency.symbol}>
-                  {currency.symbol}
-                </option>
-              ))}
-            </select>
-            <div className="input_value">
-              {result}
-            </div>
-          </div>
-
-          <button className="convert_button" onClick={convertCurrency}>
-            Converter
-          </button>
+    <div className="container">
+      <h1>Conversor de Moeda</h1>
+      <div className="title_container">
+        <p className="title_currency">Moeda</p>
+        <p className="title_value">Valor</p>
+      </div>
+      <div className="conversion_container">
+        <div className="select_from">
+          {/* Input para a moeda de origem */}
+          <input
+            className="input_from"
+            type="text"
+            value={fromCurrency}
+            onChange={(e) => setFromCurrency(e.target.value)}
+          />
+          {/* Exibição do valor da moeda de origem */}
+          <div className="input_value">{fromResult}</div>
+        </div>
+        <div className="select_to">
+          {/* Input para a moeda de destino */}
+          <input
+            className="input_to"
+            type="text"
+            value={toCurrency}
+            onChange={(e) => setToCurrency(e.target.value)}
+          />
+          {/* Exibição do valor convertido */}
+          <div className="input_value">{toResult}</div>
         </div>
 
-        <select className="conversion_history">
-          <option value="">Histórico de conversão</option>
-          {history.map((conversion, index) => (
-            <option key={index} value={conversion}>
-              {conversion}
-            </option>
-          ))}
-        </select>
+        {/* Botão de conversão */}
+        <button
+          className="convert_button"
+          onClick={convertCurrency}
+          disabled={countdown > 0}
+        >
+          {/* Texto do botão com contagem regressiva ou 'Converter' */}
+          {countdown > 0 ? `Convertendo...` : "Converter"}
+        </button>
       </div>
+
+      {/* Dropdown para exibir o histórico de conversões */}
+      <select className="conversion_history">
+        <option value="">Histórico de conversão</option>
+        {history.map((conversion, index) => (
+          <option key={index} value={conversion}>
+            {conversion}
+          </option>
+        ))}
+      </select>
     </div>
   );
-}
+};
 
 export default Converter;
